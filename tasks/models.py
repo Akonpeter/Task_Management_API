de92067f2d461befd56223122418642b58e18cf1
Akon peter <prizylink@gmail.com>
2025-12-15 10:30:58 +0100

    Implimenting the project setup

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,9 @@
 
 
 # Create your models here.
-
 from django.db import models
 from django.contrib.auth.models import User
+from project.models import Project
 
 class Task(models.Model):
     STATUS_CHOICES = (
@@ -24,3 +24,17 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+    
+
+class Task(models.Model):
+    ...
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='tasks',
+        null=True,
+        blank=True
+    )
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
