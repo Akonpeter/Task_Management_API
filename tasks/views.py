cd0bb06c7334ef00d32543ed5f61af3fdc830418
Akon peter <prizylink@gmail.com>
2025-12-16 19:36:24 +0100

    Implimentation of Custom Users Adim

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Task
 from .serializers import TaskSerializer
